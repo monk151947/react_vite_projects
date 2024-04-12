291dafacc528047a65e8d8436f82806b9891e6c6
Jagadeesh Barbari <monk151947@gmail.com>
2024-04-12 12:43:27 +0530

    react router

---
+++ 06reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+function Github() {
+  const data = useLoaderData();
+
+  return (
+    <div className="text-center m-4 bg-gray-600 text-white p-4 text-3xl">
+      Github followers: {data.followers}
+      <img src={data.avatar_url} alt="Git picture" width={300} />
+      <div className="text-center text-3xl">{data.name}</div>
+    </div>
+  );
+}
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/monk151947");
+  return response.json();
+};
