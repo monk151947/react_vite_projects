57e2eed7f81ab1eb4c66a3d5c6fa61d92db87dcb
Jagadeesh Barbari <monk151947@gmail.com>
2024-05-14 11:55:08 +0530

    hooks examples added

--- basicReact/src/App.jsx
+++ basicReact/src/App.jsx
@@ -1,15 +1,53 @@
+import { useEffect, useRef } from 'react'
 import { useState } from 'react'
 import './App.css'
+import Component1 from './components/componentOne'
 
 function App() {
   const [count, setCount] = useState(1)
  // const [value, setValue] = useState(1)
-let value = count * 10
+  const [calculation, setCalculation] = useState(0);
+  const [inputValue, setInputValue] = useState("");
+  const counter = useRef(0);
+
+
+ let value = count * 10
 
   const MultiplyByTen = () => {
    //setValue(count * 10)
    setCount(count+1)
   }
+
+  // This useeffect will run each sec  
+
+  // useEffect(()=> {
+  //   setTimeout(()=> {
+  //     setCount(count + 1)
+  //   }, 1000)
+  // })
+
+// This will run only once after page loads because we are pass empty array []
+  useEffect(()=> {
+    setTimeout(()=> {
+      setCount(count + 1)
+    }, 1000)
+  }, [])   // empty []
+
+  // another way 
+   
+  // useEffect(() => {
+  //   //Runs on the first render
+  //   //And any time any dependency value changes
+  // }, [prop, state]);
+
+  useEffect(() => {
+    setCalculation(() => count * 2);
+  }, [count]); // <- add the count variable here
+
+
+  useEffect(() => {
+    counter.current = counter.current + 1;
+  });
 
   return (
     <>
@@ -17,6 +55,14 @@ let value = count * 10
      <p>Hello React Count:  {count}</p>
      <button onClick={MultiplyByTen}>Multiply By 10</button>
      <p>Multiplied Value : {value}</p>
+     <p>Calculation Value : {calculation}</p>
+     <input
+        type="text"
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <h1>Render Count: {counter.current}</h1>
+     <Component1 />
     </>
   )
 }
